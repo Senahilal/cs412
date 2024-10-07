c7a9dee0d227c168279b8fb30659af22f30bc388
Senahilal <senab@bu.edu>
2024-10-06 23:15:38 -0400

    Updated route to registered profile info page

--- cs412/urls.py
+++ cs412/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')),
     path('restaurant/', include('restaurant.urls')),
-    path("mini/", include("mini_fb.urls")),
+    path("mini_fb/", include("mini_fb.urls")),
 ]
