274735a1321c9ba9a52aa1a60c7f3e735979a088
Senahilal <senab@bu.edu>
2024-11-12 02:25:13 -0500

    created search form

--- voter_analytics/forms.py
+++ voter_analytics/forms.py
@@ -1,3 +1,49 @@
 from django import forms
 from .models import *
 
+class SearchVotersForm(forms.Form):
+    PARTY_CHOICES = [
+        ('', 'Any'),
+        ('D', 'Democratic'),
+        ('R', 'Republican'),
+        ('CC', 'Constitution Party'),
+        ('L', 'Libertarian Party'),
+        ('T', 'Tea Party'),
+        ('O', 'Other'),
+        ('G', 'Green Party'),
+        ('J', 'Independent Party'),
+        ('Q', 'Reform Party'),
+        ('FF', 'Freedom Party'),
+    ]
+
+    party_affiliation = forms.ChoiceField(
+        choices=PARTY_CHOICES,
+        required=False,
+        label='Party Affiliation'
+    )
+
+    min_date_of_birth = forms.ChoiceField(
+        choices=[('', 'Select Year')] + [(year, year) for year in range(1900, 2025)],
+        required=False,
+        label='Born After'
+    )
+    
+    max_date_of_birth = forms.ChoiceField(
+        choices=[('', 'Select Year')] + [(year, year) for year in range(1900, 2025)],
+        required=False,
+        label='Born Before'
+    )
+
+    voter_score = forms.ChoiceField(
+        choices=[('', 'Any')] + [(i, str(i)) for i in range(6)],
+        required=False,
+        label='Voter Score'
+    )
+
+
+    # Whether they voted in specific elections (check boxes)
+    voted_v20state = forms.BooleanField(required=False, label='2020 State Election')
+    voted_v21town = forms.BooleanField(required=False, label='2021 Town Election')
+    voted_v21primary = forms.BooleanField(required=False, label='2021 Primary Election')
+    voted_v22general = forms.BooleanField(required=False, label='2022 General Election')
+    voted_v23town = forms.BooleanField(required=False, label='2023 Town Election')
--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from . models import *
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from .forms import *
+from django.views.generic import *
 from django.shortcuts import redirect
 
 # Create your views here.
@@ -12,3 +13,51 @@ class VoterListView(ListView):
     template_name = 'voter_analytics/voter_list.html'
     context_object_name = 'voters'
     paginate_by = 100  # Show 100 records per page
+
+    def get_queryset(self):
+        '''Return the filtered set of Voter records.'''
+
+        # Start with the base queryset
+        qs = super().get_queryset()
+
+        # Instantiate the form with GET parameters
+        self.form = SearchVotersForm(self.request.GET)
+
+        if self.form.is_valid():
+            # Apply filters based on form input
+            if self.form.cleaned_data['party_affiliation']:
+                qs = qs.filter(party_affiliation=self.form.cleaned_data['party_affiliation'])
+
+            if self.form.cleaned_data['min_date_of_birth']:
+                qs = qs.filter(date_of_birth__gte=self.form.cleaned_data['min_date_of_birth'])
+
+            if self.form.cleaned_data['max_date_of_birth']:
+                qs = qs.filter(date_of_birth__lte=self.form.cleaned_data['max_date_of_birth'])
+
+            if self.form.cleaned_data['voter_score']:
+                qs = qs.filter(voter_score=self.form.cleaned_data['voter_score'])
+
+            if self.form.cleaned_data['voted_v20state']:
+                qs = qs.filter(v20state=True)
+
+            if self.form.cleaned_data['voted_v21town']:
+                qs = qs.filter(v21town=True)
+
+            if self.form.cleaned_data['voted_v21primary']:
+                qs = qs.filter(v21primary=True)
+
+            if self.form.cleaned_data['voted_v22general']:
+                qs = qs.filter(v22general=True)
+
+            if self.form.cleaned_data['voted_v23town']:
+                qs = qs.filter(v23town=True)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        '''Add the form to the context.'''
+        context = super().get_context_data(**kwargs)
+        
+        # Add the form to the context
+        context['form'] = self.form  
+        return context
