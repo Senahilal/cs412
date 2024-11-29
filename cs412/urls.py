9b9932c73e7436840dea4f39ee3271e4629c1f68
Senahilal <senab@bu.edu>
2024-11-29 18:12:49 -0500

    added url for project app

--- cs412/urls.py
+++ cs412/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path("mini_fb/", include("mini_fb.urls")),
     path("voter_analytics/", include("voter_analytics.urls")),
+    path("project/", include("project.urls")),
 ]
 
 ## update project-level URLs to include media directory
